7cfd6adedb54340e1954b7aef6ecf098848be45c
Dkingofcode <doladepo128@gmail.com>
2023-01-11 17:59:21 +0000

    Written the markupa and added a few styles

---
+++ index.js
@@ -0,0 +1,33 @@
+// items 
+  const menu = [
+    {
+        id: 1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "",
+        desc: `I'm baby woke milkshakle wolf gang, i don't do nothing
+        i don't want to do. I am fearless in facing any bad nanay who 
+        tries to prove she's smarter `,
+    },
+    {
+        id: 2,
+        title: "diner double",
+        category: "lunch",
+        price: 13.99,
+        img: "",
+        desc: `Vapourize the washed waves salvage the raw skin, as they are
+        precious gems in time of gathering jiust looking at the reaper's hand
+         magic. `
+    },
+    {
+        id: 2,
+        title: "diner double",
+        category: "lunch",
+        price: 13.99,
+        img: "",
+        desc: `Vapourize the washed waves salvage the raw skin, as they are
+        precious gems in time of gathering jiust looking at the reaper's hand
+         magic. ` 
+    }
+  ]
